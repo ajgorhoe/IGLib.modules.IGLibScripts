90ead1f4a81fdd1eace63a0f071bcf8325ddeecb
Igor Grešovnik <gresovnik@gmail.com>
2025-02-28 21:33:50 +0100

    Added 2 settings files with repository parameters for cloning/updating

---
+++ psutils/ReopositoryManagement/SettingsIGLibCore.ps1
@@ -0,0 +1,14 @@
+
+# Update / Clone settings for the IGLibCore repository
+
+$RepositoryDirectory = "IGLibCore"
+$RepositoryRef = $main
+$RepositoryAddress = "https://github.com/ajgorhoe/IGLib.modules.IGLibCore.git"
+$RepositoryRemote = "origin"
+$RepositoryAddressSecondary = $null
+$RepositoryRemoteSecondary = $null
+$RepositoryAddressTertiary = "d:\backup_sync\bk_code\git\ig\misc\iglib_modules\IGLibCore\"
+$RepositoryRemoteTertiary = "local"
+$RepositoryThrowOnErrors = $null
+$RepositoryDefaultFromVars = $null
+
---
+++ psutils/ReopositoryManagement/SettingsIGLibScript.ps1
@@ -0,0 +1,14 @@
+
+# Update / Clone settings for the IGLibScripts repository
+
+$RepositoryDirectory = "IGLibScripts"
+$RepositoryRef = $main
+$RepositoryAddress = "https://github.com/ajgorhoe/IGLib.modules.IGLibScripts.git"
+$RepositoryRemote = "origin"
+$RepositoryAddressSecondary = $null
+$RepositoryRemoteSecondary = $null
+$RepositoryAddressTertiary = "d:\backup_sync\bk_code\git\ig\misc\iglib_modules\IGLibScripts\"
+$RepositoryRemoteTertiary = "local"
+$RepositoryThrowOnErrors = $null
+$RepositoryDefaultFromVars = $null
+
