ab5330e189c4fdcb03dc4168f5af66b7e421ed68
Igor Grešovnik <gresovnik@gmail.com>
2025-02-28 23:16:37 +0100

    Added a file with test samples for UpdateOrCloneRepository.ps1

---
+++ psutils/ReopositoryManagement/TestUpdateOrCloneRepository.ps1
@@ -0,0 +1,19 @@
+
+# This contains PowerShell commands for testing the UpdateOrCloneRepository.ps1.
+# Open the PowerShell CLI in the directory of this script and copy-paste commands
+# from this file, then verify the results.
+
+# Cloning repository without specifying the checkout branch, into nested directory:
+# Before, either remove the clone directory, or let it exist.
+# Both if directory does not exist or it exists but it is empty,
+# the remote repository should be cloned into the directory.
+./UpdateOrCloneRepository.ps1 -Directory "repos/IGLibScripts" `
+    -address "https://github.com/ajgorhoe/IGLib.modules.IGLibScripts.git"
+
+# Cloning or updating with a different branch than the default one:
+# Remove the repo directory before executing, or keep it with another ref
+# check out to observe the effect.
+# Expected: in all cases the specified ref should be checked out. 
+./UpdateOrCloneRepository.ps1 -Directory "IGLibScripts" `
+    -address "https://github.com/ajgorhoe/IGLib.modules.IGLibScripts.git" `
+    -ref "release/latestrelease"
