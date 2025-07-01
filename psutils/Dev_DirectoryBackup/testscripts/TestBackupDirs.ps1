5634358501076d4ebbfcd149c1ff12287e007912
Igor Grešovnik <gresovnik@gmail.com>
2025-07-01 16:25:18 +0200

    First test added, created a helper file for storing parts of code in discussion

--- psutils/Dev_DirectoryBackup/testscripts/TestBackupDirs.ps1
+++ psutils/Dev_DirectoryBackup/testscripts/TestBackupDirs.ps1
@@ -6,10 +6,16 @@
 # Recursively remove a directory:
 #   Remove-Item -Path "path\to\directory" -Recurse -Force
 
-# Backup a directory (BackupSourceBasic) with 
+# Backup a directory (BackupSourceBasic) with usual mode (without the InPlace 
+# option) and with relative paths
 Remove-Item -Path "backups/BackupSourceBasic" -Recurse -Force  # first, remove the backup if it exists
-. ../BackupDir.ps1 ./sourcedirs/BackupSourceBasic ./backups/BackupSourceBasic
+. ../BackupDir.ps1 "./sourcedirs/BackupSourceBasic" "./backups/BackupSourceBasic"
 
+
+# Backup a directory (BackupSourceBasic) with usual mode (without the InPlace 
+# option) and with ABSOLUTE PATHS
+Remove-Item -Path "backups/BackupSourceBasic" -Recurse -Force  # first, remove the backup if it exists
+. ../BackupDir.ps1 "e:\wsorkspace\ws\other\iglibmodules\IGLibScripts\psutils\Dev_DirectoryBackup\testscripts\sourcedirs/BackupSourceBasic" "e:\wsorkspace\ws\other\iglibmodules\IGLibScripts\psutils\Dev_DirectoryBackup\testscripts/backups/BackupSourceBasic"
 
 # =================================================
 
