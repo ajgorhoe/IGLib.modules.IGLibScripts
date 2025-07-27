b7baddd66a14d4428855d601cd2dcbc9d8a0f956
Igor Grešovnik <gresovnik@gmail.com>
2025-07-27 15:21:56 +0200

    Improved removal scripts

--- __trash/removetrash.ps1
+++ __trash/removetrash.ps1
@@ -2,10 +2,9 @@
 # Removes contents of the trash_contents/ subdirectory.
 # Also creates trash_contents/ and save/ subdirectories if they don't exist.
 
-Write-Host
-Write-Host "REMOVING contents of the trash_contents/ directory"
-Write-Host "  and creating save/ and trash_contents/ subdirectories if they"
-Write-Host "  don't exist..."
+Write-Host "`n"
+Write-Host "REMOVING contents of the trash_contents/ directory;"
+Write-Host "  creating save/ and trash_contents/ subdirectories if they don't exist..."
 
 
 
@@ -20,18 +19,20 @@ Write-Host "  $scriptDir"
 $saveDir =   $(Join-Path $scriptDir "save")
 $removeDir = $(Join-Path $scriptDir "trash_contents")
 
-Write-Host Removed dir. path:
-Write-Host $removeDir
+# Write-Host Removed dir. path:
+# Write-Host "  $removeDir"
 
 # Remove the complete trash_contents/ sub-directort:
-# Remove-Item -Recurse -Force -Path $removeDir
+Write-Host Removing trash_contents/ ...
+Remove-Item -Recurse -Force -Path $removeDir
 
-Write-Host "  ... contents of trash_contents/ removed."
+Write-Host "  ... trash_contents/ removed."
 
 # If the save/ and trash_contents/ subdirectories do not exist, create them:
-# New-Item -ItemType Directory -Force -Path $saveDir
-# New-Item -ItemType Directory -Force -Path $removeDir
+
+New-Item -ItemType Directory -Force -Path $saveDir > $null
+New-Item -ItemType Directory -Force -Path $removeDir > $null
 
 Write-Host "  ... subdirectories created."
-Write-Host
+Write-Host "`n"
 
